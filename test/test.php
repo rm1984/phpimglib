c6802eb17fb3caeec3df9fe7b91e088721e0de1a
Riccardo Mollo <rmollo@csp.it>
2015-06-11 12:06:19 +0200

    ...

--- test/test.php
+++ test/test.php
@@ -11,20 +11,20 @@
 				<th colspan="2" class="test_table_row">Test 1</th>
 			</tr>
 <?php
-	$filename_image1 = "image1.jpg";
-	$realpath_image1 = realpath($filename_image1);
-	$image1 = getImage($filename_image1);
+	$filename_image = "image1.jpg";
+	$realpath_image = realpath($filename_image);
+	$image = getImage($filename_image);
 
-	$mimetype = getImageMIMEType($filename_image1);
-	list($width, $height) = getImageWidthAndHeight($image1);
+	$mimetype = getImageMIMEType($filename_image);
+	list($width, $height) = getImageWidthAndHeight($image);
 ?>
 			<tr>
 				<td class="test_table_row">Image</td>
-				<td class="test_table_row"><img src="<?php echo $filename_image1; ?>"/></td>
+				<td class="test_table_row"><img src="<?php echo $filename_image; ?>"/></td>
 			</tr>
 			<tr>
 				<td class="test_table_row">Filename</td>
-				<td class="test_table_row"><?php echo $realpath_image1; ?></td>
+				<td class="test_table_row"><?php echo $realpath_image; ?></td>
 			</tr>
 			<tr>
 				<td class="test_table_row">Width x Height (in pixels)</td>
@@ -36,11 +36,49 @@
 			</tr>
 			<tr>
 				<td class="test_table_row">Histogram (based on luminance)</td>
-				<td class="test_table_row"><?php getHistogram($image1, LUMINANCE); ?></td>
+				<td class="test_table_row"><?php getHistogram($image, LUMINANCE); ?></td>
 			</tr>
 			<tr>
 				<td class="test_table_row">Histogram (based on black and white)</td>
-				<td class="test_table_row"><?php getHistogram($image1, BLACKWHITE); ?></td>
+				<td class="test_table_row"><?php getHistogram($image, BLACKWHITE); ?></td>
+			</tr>
+		</table>
+
+		<table class="test_table">
+			<tr>
+				<th colspan="2" class="test_table_row">Test 1</th>
+			</tr>
+<?php
+	$filename_image = "image2.png";
+	$realpath_image = realpath($filename_image);
+	$image = getImage($filename_image);
+
+	$mimetype = getImageMIMEType($filename_image);
+	list($width, $height) = getImageWidthAndHeight($image);
+?>
+			<tr>
+				<td class="test_table_row">Image</td>
+				<td class="test_table_row"><img src="<?php echo $filename_image; ?>"/></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">Filename</td>
+				<td class="test_table_row"><?php echo $realpath_image; ?></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">Width x Height (in pixels)</td>
+				<td class="test_table_row"><?php echo $width .  " x " . $height; ?></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">MIME type</td>
+				<td class="test_table_row"><?php echo $mimetype; ?></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">Histogram (based on luminance)</td>
+				<td class="test_table_row"><?php getHistogram($image, LUMINANCE); ?></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">Histogram (based on black and white)</td>
+				<td class="test_table_row"><?php getHistogram($image, BLACKWHITE); ?></td>
 			</tr>
 		</table>
 	</body>
