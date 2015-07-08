29c5e1d85e539b6053e9df4fc865d9bd9a9ed141
Riccardo Mollo <rmollo@csp.it>
2015-07-08 15:30:48 +0200

    ...

--- test/test.php
+++ test/test.php
@@ -99,5 +99,52 @@
 				<td class="test_table_row"><?php boost($image); ?></td>
 			</tr>
 		</table>
+		<br/>
+		<table class="test_table">
+			<tr>
+				<th colspan="2" class="test_table_row">Test 3</th>
+			</tr>
+<?php
+	$filename_image = "image3.jpg";
+	$realpath_image = realpath($filename_image);
+	$image = getImage($filename_image);
+
+	$mimetype = getImageMIMEType($filename_image);
+	list($width, $height) = getImageWidthAndHeight($image);
+	list($dom_r, $dom_g, $dom_b) = getDominantColor($image);
+?>
+			<tr>
+				<td class="test_table_row">Image</td>
+				<td class="test_table_row"><img src="<?php echo $filename_image; ?>"/></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">Filename</td>
+				<td class="test_table_row"><?php echo $realpath_image; ?></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">Width x Height (in pixels)</td>
+				<td class="test_table_row"><?php echo $width .  " x " . $height; ?></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">MIME type</td>
+				<td class="test_table_row"><?php echo $mimetype; ?></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">Histogram (based on luminance)</td>
+				<td class="test_table_row"><?php getHistogram($image, LUMINANCE); ?></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">Histogram (based on black and white)</td>
+				<td class="test_table_row"><?php getHistogram($image, BLACKWHITE); ?></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">Dominant color</td>
+				<td class="test_table_row"><div class="foo" style="background-color: <?php echo "rgb(" . $dom_r . ", " . $dom_g . ", " . $dom_b . ");"; ?>"><?php echo "RGB (" . $dom_r . ", " . $dom_g . ", " . $dom_b . ")"; ?></div></td>
+			</tr>
+			<tr>
+				<td class="test_table_row">Boosted image</td>
+				<td class="test_table_row"><?php boost($image); ?></td>
+			</tr>
+		</table>
 	</body>
 </html>
